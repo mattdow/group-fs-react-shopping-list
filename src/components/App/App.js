c2183657bd9f0d3fcadc2ee7bb7dce6830f871d1
mbmosman <mbMosman@users.noreply.github.com>
2020-12-02 20:06:24 -0600

    initial add

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Songs!</h1>
+        </header>
+        <main>
+          <p>Under Construction...</p>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
