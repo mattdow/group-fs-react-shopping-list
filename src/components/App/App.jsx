d001f1efae1ceed26a4d896238a0063f1f07824f
Matt Dow <matt.dow.vo@gmail.com>
2021-10-21 14:20:33 -0500

    Minor edits

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useEffect, useState } from 'react';
 
 import Header from '../Header/Header.jsx'
 import './App.css';
 import GroceryList from '../GroceryList/GroceryList.jsx'
+
 
 
 function App() {
--- src/components/GroceryList/GroceryList.jsx
+++ src/components/GroceryList/GroceryList.jsx
@@ -1,4 +1,4 @@
-import Axios from 'axios';
+
 
 
 function GroceryList({groceryList, getGroceryList}) {
