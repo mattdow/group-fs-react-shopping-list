ac594d561242453f69fde7da3dffcb30f7994e41
Matt Dow <matt.dow.vo@gmail.com>
2021-10-21 11:16:36 -0500

    Added GET request to server

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -4,5 +4,17 @@ const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
 
+// GET current grocery list
+router.get('/', (req, res) => {
+    const queryText = 'SELECT * FROM list';
+    pool.query(queryText)
+    .then((result) => {
+        res.send(result.rows);
+    })
+    .catch((error) => {
+        console.log('Error in GET request', error);
+    }); 
+});
+
 
 module.exports = router;
