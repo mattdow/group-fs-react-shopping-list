8e71963ff5a5784e4ae073273856ed0ade93db51
Matt Dow <matt.dow.vo@gmail.com>
2021-10-22 09:45:24 -0500

    Added buttons

--- src/components/GroceryItem/GroceryItem.jsx
+++ src/components/GroceryItem/GroceryItem.jsx
@@ -7,10 +7,18 @@ function GroceryItem ({item, getGroceryList}) {
 
 
     return (
+    
         <div className="grocery-item">
             <p>{item.item}</p>
             <p>{item.quantity} {item.unit}</p>
+            <p>
+            <button>Buy</button>
+            <button>Remove</button>
+            </p>
         </div>
+        
+        
+    
 
     )
 
