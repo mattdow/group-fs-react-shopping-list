ddbddf24ff4ba0629b28d19f28b76292de509d7f
Matt Dow <matt.dow.vo@gmail.com>
2021-10-22 10:28:11 -0500

    Added Axios methods

--- src/components/GroceryItem/GroceryItem.jsx
+++ src/components/GroceryItem/GroceryItem.jsx
@@ -4,10 +4,34 @@ import './GroceryItem.css';
 
 function GroceryItem ({item, getGroceryList}) {
 
-    // const deleteItem = () => {
-    //     let idToDelete = item.id;
-    //     console.log();
-    // }
+    const deleteItem = () => {
+        let idToDelete = item.id;
+        console.log('Clicked delete', item.item);
+        axios({
+            method: 'DELETE',
+            url: `/list/${idToDelete}`
+        }).then((response) => {
+            console.log('DELETE response is', response);
+            getGroceryList();
+        }).catch((error) => {
+            console.log('DELETE error', error);
+        })
+    }
+
+    const purchaseItem = () => {
+        let idToPurchase = item.id;
+        console.log('Clicked Buy', item.item);
+        axios({
+            method: 'PUT',
+            url: `/list/${idToPurchase}`
+        }).then((response) => {
+            console.log('PUT purchase response is', response);
+            getGroceryList();
+        }).catch((error) => {
+            console.log('PUT purchase error', error);
+        })
+
+    }
 
 
     return (
@@ -15,13 +39,14 @@ function GroceryItem ({item, getGroceryList}) {
         <div className="grocery-item">
             <p>{item.item}</p>
             <p>{item.quantity} {item.unit}</p>
+
             <p>
                 {item.purchased
                     ? `Purchased`
-                    : <><button>Buy</button><button>Remove</button></>
+                    : <><button onCLick={purchaseItem}>Buy</button><button onClick={deleteItem}>Remove</button></>
                 }   
-            </p>
-        </div>
+            </p> 
+        </div> 
     )
 }
 
